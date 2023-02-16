71c9d5a64363d6790b3b9aa2832d050ae8402c71
david </>
2023-02-16 20:32:33 +0300

    Logs

--- app/src/main/java/com/example/pr1/MainActivity.java
+++ app/src/main/java/com/example/pr1/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = this.getClass().getSimpleName();
     public void displayToast(String text){
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
@@ -18,31 +19,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello World");
         displayToast("onCreate");
+        Log.i(TAG, "InformationCreate");
+        Log.w(TAG, "Warning");
+        Log.e(TAG, "Error");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Verbode");
     }
     @Override
     protected void onStart() {
         super.onStart();
         displayToast("onStart");
+        Log.i(TAG, "InformationStart");
+        Log.w(TAG, "Warning");
+        Log.e(TAG, "Error");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Verbode");
     }
     @Override
     protected void onStop() {
         super.onStop();
         displayToast("onStop");
+        Log.i(TAG, "InformationStop");
+        Log.w(TAG, "Warning");
+        Log.e(TAG, "Error");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Verbode");
     }
     @Override
     protected void onDestroy() {
         isFinishing();
         displayToast("onDestroy");
         super.onDestroy();
+        Log.i(TAG, "InformationDestroy");
+        Log.w(TAG, "Warning");
+        Log.e(TAG, "Error");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Verbode");
     }
     @Override
     protected void onPause() {
         super.onPause();
         displayToast("onPause");
+        Log.i(TAG, "InformationPause");
+        Log.w(TAG, "Warning");
+        Log.e(TAG, "Error");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Verbode");
     }
     @Override
     protected void onResume() {
         super.onResume();
         displayToast("onResume");
+        Log.i(TAG, "InformationResume");
+        Log.w(TAG, "Warning");
+        Log.e(TAG, "Error");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Verbode");
     }
 }
